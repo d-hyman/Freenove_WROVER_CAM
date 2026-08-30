b9e0e31e8b02a8c43185a1c5d1ec968506909642
Daniel Hyman <danielHyman20@gmail.com>
2026-08-29 21:15:56 -0400

    ESP32 Camera monitor
    When platformio is built and uploaded to your ESP32, the attached ESP32 camera can be monitored on the specific https IP address

---
+++ include/board_config.h
@@ -0,0 +1,11 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+// ===================
+// Select camera model
+// ===================
+#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+
+#include "camera_pins.h"
+
+#endif  // BOARD_CONFIG_H
